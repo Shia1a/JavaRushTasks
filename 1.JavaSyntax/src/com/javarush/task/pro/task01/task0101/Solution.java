3d4542b0079ba08fc7440f4e47405bebc5daa95f
Tatyana <ttarhiv@yandex.ru>
2023-01-25 11:16:57 +0300

    add solution JavaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task01.task0101;
+
+/* 
+Первая программа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.print ( "Диего + Амиго = Друзья" );
+
+    }
+}
