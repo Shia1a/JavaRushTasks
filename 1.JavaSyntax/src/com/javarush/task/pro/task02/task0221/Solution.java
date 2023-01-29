a11057ca10bd83da2e46327af83ad0c9a4bb3c3b
Tatyana <ttarhiv@yandex.ru>
2023-01-29 16:55:20 +0300

    add solution JavaSyntax task 0221

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0221/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0221;
+
+public class Solution {
+    public static void main(String[] args) {
+        String longitude = "-74.042";
+        String numberS = "87";
+        String numberF = "89";
+        String comma = ",";
+
+        System.out.println("40.7281" + "89" + "," + "-74.042" + "89" + "87");
+    }
+}
