056775d399b60fafd97906dc1a29f67957018c6e
Tatyana <ttarhiv@yandex.ru>
2023-01-29 16:54:28 +0300

    add solution JavaSyntax task 0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       String name = "Dolly", date = "today", time = "afternoon", noun = "sun", adjective = "warm", verb = "shines";
+
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
