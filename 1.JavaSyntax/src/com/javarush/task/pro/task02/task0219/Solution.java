a8316d7da76bc1f8d4a794a58831fb472967645e
Tatyana <ttarhiv@yandex.ru>
2023-01-29 16:54:41 +0300

    add solution JavaSyntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int id = 8;
+       int age = 65;
+       String documents = "passports";
+       String behavior = "bad";//напишите тут ваш код
+    }
+}
