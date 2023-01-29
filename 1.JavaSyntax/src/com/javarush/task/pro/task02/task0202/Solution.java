8bf628e8ddb3924093c8e6971135fd8c5eeb4f11
Tatyana <ttarhiv@yandex.ru>
2023-01-29 10:11:29 +0300

    add solution JavaSyntax task 0202

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = 0;
+        int meters = 243/100; //напишите тут ваш код
+
+        System.out.println(meters);
+    }
+}
