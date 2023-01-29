3c13bcec12f9f21d672bb585e02a1e12d042518a
Tatyana <ttarhiv@yandex.ru>
2023-01-29 10:11:50 +0300

    add solution JavaSyntax task 0203

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0203;
+
+/* 
+Последняя цифра числа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 546;
+
+        int lastDigit = c;
+        int c = 546 % 10
+
+        //напишите тут ваш код
+
+        System.out.println(lastDigit);
+    }
+}
