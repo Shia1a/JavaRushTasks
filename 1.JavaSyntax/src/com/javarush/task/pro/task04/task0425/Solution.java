e3e9d4967c7807693031aff50f749f401bb66800
Tatyana <ttarhiv@yandex.ru>
2023-03-04 17:59:11 +0300

    add solution JavaSyntax task 0425

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task04.task0425;
+
+/* 
+Диггер (1)
+*/
+
+public class Solution {
+    public static String bug = "🐛";
+    public static String robot = "🤖";
+    public static String land = "🌍";
+    public static String death = "💀";
+    public static String hole = " ";
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+            for (int m = 0; m <= 9; m++) {
+                System.out.print(land);
+            }
+                System.out.println();
+            }
+        }
+  }
+
