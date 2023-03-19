a45f9b6e51584fbe9a79e6c32019555e30e39058
Tatyana <ttarhiv@yandex.ru>
2023-03-19 17:58:59 +0300

    add solution JavaSyntax task 0416

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -8,7 +8,13 @@ Share a Coke
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner (System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        double d = a*1.0/b;
+        System.out.println (d);
+
+
 
     }
 }
