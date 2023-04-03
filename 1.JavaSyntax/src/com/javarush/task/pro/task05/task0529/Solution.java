b9ae8defa3c27648e45abae8a33875a3406adb6f
Tatyana <ttarhiv@yandex.ru>
2023-04-03 20:01:02 +0300

    add solution JavaSyntax task 0529

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -11,22 +11,23 @@ public class Solution {
     public static String empty = "_";
     public static String hit = "🎯";
     public static int width = 30;
-    public static int height = 10;
-    public static String[][] field = new String[height][width];
+    public static int height=10;
+    public static String[][]field= new String [height][width];
+
 
     public static void main(String[] args) {
-        for (int i = 0; i < field.length; i++) {
-            Arrays.fill(field[i], empty);
+      for (int i=0;i< field.length;i++){
+          Arrays.fill(field[i],empty);
+                     for (int j=0;j<field[i].length;j++) {
+                         System.out.print(field[i][j]);
+                     }
+                         System.out.println();
+                     }
 
-        }
+              }
 
-        for(int i = 0; i < field.length; i++){
-            for(int j = 0; j < field[i].length;j++){
-                System.out.print(field[i][j]);
-            }
-            System.out.println();
-        }
+          }
+
+          //напишите тут ваш код
 
 
-    }
-}
